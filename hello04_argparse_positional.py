591c6e3ad6a2f1c146fdbe56d3f4da1575df5f77
Shankar <shankarnarayanb@gmail.com>
2021-04-27 18:27:58 +0100

    Inital commit

---
+++ hello04_argparse_positional.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Purpose: Say hello
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+
+parser.add_argument('name', help='Name to greet')
+
+
+args = parser.parse_args()
+print(args)
+print('Hello, ' + args.name + '!')
